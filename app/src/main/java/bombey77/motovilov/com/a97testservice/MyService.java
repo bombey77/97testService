3b084b4b21e1c38d817ff310a24a4885ab5ed522
Motovilov Roman <bombey131@gmail.com>
2017-07-24 21:28:43 +0200

    create service

---
+++ app/src/main/java/bombey77/motovilov/com/a97testservice/MyService.java
@@ -0,0 +1,53 @@
+package bombey77.motovilov.com.a97testservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Ромашка on 23.07.2017.
+ */
+
+public class MyService extends Service {
+
+    private static final String LOG = "myLogs";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG, "onBind");
+        return null;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.d(LOG, "onRebind");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(LOG, "onUnbind");
+        return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG, "onDestroy");
+    }
+}
